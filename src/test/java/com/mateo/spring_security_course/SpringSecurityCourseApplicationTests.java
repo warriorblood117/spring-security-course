8c161009613c5b49df14510b463809b04b684721
warriorblood117 <warriorblood117@gmail.com>
2024-09-24 13:24:15 -0500

    first structure

---
+++ src/test/java/com/mateo/spring_security_course/SpringSecurityCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateo.spring_security_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
